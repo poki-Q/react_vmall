a767e630dd4dffdf418026bef04fbe07b1def0e9
poki <577367252@qq.com>
2020-05-05 14:19:35 +0800

    list-2

--- src/components/loading.js
+++ src/components/loading.js
@@ -4,7 +4,7 @@ export default class loading extends Component {
     render() {
         return (
             <div>
-                list..1
+                list..1..2
                 index..1...2
             </div>
         )
