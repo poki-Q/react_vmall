57b0c50c290be841ffcc5f0fbe2dc12a26ef0b3c
poki <577367252@qq.com>
2020-05-05 13:54:24 +0800

    clear comppp

--- src/components/loading.js
+++ src/components/loading.js
@@ -4,7 +4,7 @@ export default class loading extends Component {
     render() {
         return (
             <div>
-                index...
+                
             </div>
         )
     }
