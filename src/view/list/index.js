92b50b553cd89d5d5e31fda564720c911275e5ec
poki <577367252@qq.com>
2020-05-05 14:02:33 +0800

    list-2

--- src/view/list/index.js
+++ src/view/list/index.js
@@ -4,7 +4,7 @@ export default class index extends Component {
     render() {
         return (
             <div>
-                list.....1
+                list.....1.....2
             </div>
         )
     }
