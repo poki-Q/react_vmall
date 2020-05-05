5793a5a07815aa5f8aff42424fb54bed0c74e312
poki <577367252@qq.com>
2020-05-05 14:05:27 +0800

    index-1

--- src/components/loading.js
+++ src/components/loading.js
@@ -4,7 +4,7 @@ export default class loading extends Component {
     render() {
         return (
             <div>
-                
+                index..1
             </div>
         )
     }
